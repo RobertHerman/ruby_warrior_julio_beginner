f4d7f809d946f765718db5df4b1ebcecb7c0787f
Robert Herman <rherman@shaw.ca>
2013-05-05 01:02:09 -0600

    End of Level 4

--- player.rb
+++ player.rb
@@ -1,16 +1,47 @@
 class Player
-  MAX_HEALTH = 20
-  MIN_HEALTH = 6
- 
+  def initialize
+    @max_health = 20
+    @health = @max_health
+    @min_health_to_fight = 12
+    @under_attack;
+  end
+  
+  def under_attack?
+    return @under_attack
+  end
+
+  def turn_start(warrior)
+    @under_attack = @health > warrior.health
+  end
+
+  def turn_end(warrior)
+    @health = warrior.health
+  end
+
   def play_turn(warrior)
-    if warrior.feel.empty?
-      if warrior.health >  MIN_HEALTH
+    turn_start(warrior)
+    determine_strategy(warrior)    
+    turn_end(warrior)
+  end
+
+  def determine_strategy(warrior)
+    if under_attack? == true
+      if warrior.feel.empty?
         warrior.walk!
       else
+        warrior.attack!
+      end
+      return
+    end
+
+    if warrior.feel.empty? == true
+      if warrior.health < @min_health_to_fight
         warrior.rest!
+      else
+        warrior.walk!
       end
     else
-      warrior.attack!
+        warrior.attack!
     end
   end
 end
