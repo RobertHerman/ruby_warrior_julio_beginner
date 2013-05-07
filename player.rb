777fd7a559bed6250ae3763668f021f89c47cc0d
Robert Herman <rherman@shaw.ca>
2013-05-06 21:13:38 -0600

    End of level 5 - Score 303

--- player.rb
+++ player.rb
@@ -2,7 +2,7 @@ class Player
   def initialize
     @max_health = 20
     @health = @max_health
-    @min_health_to_fight = 12
+    @min_health_to_fight = 13
     @under_attack;
   end
   
@@ -25,16 +25,17 @@ class Player
   end
 
   def determine_strategy(warrior)
-    if under_attack? == true
-      if warrior.feel.empty?
-        warrior.walk!
-      else
-        warrior.attack!
-      end
+    if under_attack?
+      engage_enemy(warrior)
       return
     end
 
-    if warrior.feel.empty? == true
+    if warrior.feel.captive?
+      warrior.rescue!
+      return
+    end
+
+    if warrior.feel.empty?
       if warrior.health < @min_health_to_fight
         warrior.rest!
       else
@@ -42,6 +43,15 @@ class Player
       end
     else
         warrior.attack!
+    end
+  end
+
+  def engage_enemy(warrior)
+    # if the enemy is an archer then there is a gap to close
+    if warrior.feel.empty?
+      warrior.walk!
+    else
+      warrior.attack!
     end
   end
 end
