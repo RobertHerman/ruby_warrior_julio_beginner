1551e01b6f5a2912b7126aef6a52406490b73fbf
Robert Herman <robert.herman@gettyimages.com>
2013-05-28 07:43:01 -0600

    made warrior an instance variable

--- player.rb
+++ player.rb
@@ -4,43 +4,45 @@ class Player
     @health = @MAX_HEALTH
     @MIN_HEALTH_TO_FIGHT = 13
     @under_attack;
+    @warrior;
   end
   
   def under_attack?
     return @under_attack
   end
 
-  def turn_start(warrior)
-    @under_attack = @health > warrior.health
+  def turn_start()
+    @under_attack = @health > @warrior.health
   end
 
-  def turn_end(warrior)
-    @health = warrior.health
+  def turn_end()
+    @health = @warrior.health
   end
 
   def play_turn(warrior)
-    turn_start(warrior)
-    determine_strategy(warrior)    
-    turn_end(warrior)
+    @warrior = warrior
+    turn_start()
+    determine_strategy()    
+    turn_end()
   end
 
-  def determine_strategy(warrior)
-    return engage_enemy(warrior) if under_attack?
-    return warrior.rescue! if warrior.feel.captive?
-    return warrior.attack! unless warrior.feel.empty?
-    if warrior.health < @MIN_HEALTH_TO_FIGHT
-      warrior.rest!
+  def determine_strategy()
+    return engage_enemy() if under_attack?
+    return @warrior.rescue! if @warrior.feel.captive?
+    return @warrior.attack! unless @warrior.feel.empty?
+    if @warrior.health < @MIN_HEALTH_TO_FIGHT
+      @warrior.rest!
     else
-      warrior.walk!
+      @warrior.walk!
     end
   end
 
-  def engage_enemy(warrior)
+  def engage_enemy()
     # if the enemy is an archer then there is a gap to close
-    if warrior.feel.empty?
-      warrior.walk!
+    if @warrior.feel.empty?
+      @warrior.walk!
     else
-      warrior.attack!
+      @warrior.attack!
     end
   end
 end
