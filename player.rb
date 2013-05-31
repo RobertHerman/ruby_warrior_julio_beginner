bbf7544c8a96c4783b342e47b42af61a3f349c45
Robert Herman <robert.herman@gettyimages.com>
2013-05-31 10:56:39 -0600

    refactor - get rid of aggression factor

--- player.rb
+++ player.rb
@@ -3,7 +3,6 @@ class Player
     @previous_health = @MAX_HEALTH = 20
     @MIN_HEALTH_TO_FIGHT = 10
     @in_retreat = false
-    @aggression_factor = 0
   end
   
   def under_attack?
@@ -37,12 +36,11 @@ class Player
   end
  
   def fight_or_flee()
-    return engage_enemy() if @warrior.health >= (@MIN_HEALTH_TO_FIGHT - @aggression_factor)
+    return engage_enemy() if @warrior.health >= @MIN_HEALTH_TO_FIGHT
     retreat()
   end
 
   def retreat()
-    @aggression_factor = 0
     return @warrior.walk!(:backward) if under_attack?
     @warrior.rest!
   end
@@ -50,13 +48,12 @@ class Player
   def engage_enemy()
     if @space.empty?  #if this is an archer we have to be more aggressive and close the gap
       @warrior.walk!
-      @aggression_factor = 4
     else
       @warrior.attack!
     end
   end
 
   def healthy_enough_to_fight?
-    @warrior.health >= (@MIN_HEALTH_TO_FIGHT - @aggression_factor)
+    @warrior.health >= @MIN_HEALTH_TO_FIGHT
   end
 end
